2d634ed2ec130008cde2c57ca4df6bb302c0b920
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-09-07 17:16:10 +0300

    2024 edition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+/*debug_assertions is by default Enabled for non-release builds
+and disabled for release builds.
+#[cfg(debug_assertions)]
+{
+    if x > 40 {
+        log::info!("x is {x}"); //This works fine.
+    }
+
+    //panic!("TEST panic"); //This also works-- we get a backtrace! But the MCU restarts automatically for us.
+}
+
+A better approach (if you just need to conditionally complie in log messages)
+is to just adjust the log-level allowed for release builds by setting the cargo-feature you want.
+see https://docs.rs/log/latest/log/#compile-time-filters .
+*/
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,7 +23,7 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
     let mut x = 7;
     let y = 35;
-    x +=y;
+    x += y;
 
     log::info!("Hello, world!");
 
