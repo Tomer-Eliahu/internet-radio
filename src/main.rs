1a0749b50f19fa97ea8134264e9884ef5e228a5f
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-09-09 21:58:12 +0300

    IO expander driver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::time::Duration;
+
+#[allow(unused)]
+use embedded_svc::{
+    http::{Method, client::Client},
+    io::{Read, Write},
+};
+
+//Note esp_idf_svc re-exports esp_idf_hal so we can just use that hal from here.
+#[allow(unused)]
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{
+        delay::FreeRtos, //if not using embassy timer
+        i2c::{I2cConfig, I2cDriver},
+        io::EspIOError,
+        prelude::{Peripherals,*}
+    },
+    http::client::{Configuration, EspHttpConnection},
+    wifi::{AsyncWifi, EspWifi},
+};
+
+#[allow(unused)]
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+
+use crate::led::LedDriver;
+
 /*debug_assertions is by default Enabled for non-release builds
 and disabled for release builds.
 #[cfg(debug_assertions)]
@@ -15,13 +42,61 @@ is to just adjust the log-level allowed for release builds by setting the cargo-
 see https://docs.rs/log/latest/log/#compile-time-filters .
 */
 
-fn main() {
+//Note: to use the 2 system LEDs (that are user programmable) I either
+// need to use I2C to talk to the IO expander peripheral to set its pins (it holds the LEDs)
+// OR use this C thing https://github.com/espressif/esp-bsp/blob/master/bsp/esp32_s3_korvo_2/API.md#bulb-leds
+// (maybe I can call into that from Rust??)
+
+//######## JUST use the I2C driver as shown below (it implements the I2C trait of embedded hal) ############
+//The io expander is TCA9554A (there is a *partial* driver for it [async only] in Rust)-- but I can write my own
+//(not too much work;
+//can even make it a sync implementation of embedded hal I2C trait and contribute it as a pull request
+//to https://github.com/ladvoc/tca9554-rs?tab=readme-ov-file).
+//From the Korvo schematic I2C Address (of this io expander)：0'b 0111 000x .
+//Note that if this address is wrong, then this is the right one:
+// https://esp32.com/viewtopic.php?t=28578
+// Recall some devices have a WhoAmI register ( To check if the device is addressed correctly,
+//read its device ID and print the value.)
+//######## JUST use the I2C driver as shown below (it implements the I2C trait of embedded hal) ############
+
+//Note: I will also need to use this IO expander to use the LCD anyway (but *just* for initilization!)...
+//The LCD uses SPI and I2C (primarly SPI-- which is much faster due to underlying hardware differences [this
+//is a general fact])
+
+//Note: from esp-idf-hal README.md: The following pins are not recommended for use by you:
+//ESP32-S3:	26 - 32, 33 - 37* (*= When using Octal Flash and/or Octal PSRAM)
+
+//Note from googling around (https://esp32.com/viewtopic.php?t=34179),
+// it appears for the esp-idf you can pick whichever GPIO pins you want
+//to be primary and secondary I2C.
+
+/*You probably want to do something like this (NOTE this is not for MY BOARD!)
+    let sda = peripherals.pins.gpio10;
+    let scl = peripherals.pins.gpio8;
+    let i2c = peripherals.i2c0;
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let i2c = I2cDriver::new(i2c, sda, scl, &config)?;
+
+*/
+
+fn main() -> ! {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let sda = peripherals.pins.gpio17; //serial data line
+    let scl = peripherals.pins.gpio18; //serial clock line
+    let i2c = peripherals.i2c0;
+    let config = I2cConfig::new().baudrate(led::BAUDRATE_STANDARD.into()); //Todo: attempt putting this at fast
+    let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
+    let mut led_driver = LedDriver::build(i2c);
+    //Todo: flash this and see the LEDs turn on
+
     let mut x = 7;
     let y = 35;
     x += y;
@@ -29,5 +104,113 @@ fn main() {
     log::info!("Hello, world!");
 
     log::info!("x is {x}"); //This works fine.
-    panic!("TEST panic");
+
+    loop {
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
+
+///The LEDs are on the IO expander peripheral: [the TCA9554A](https://www.ti.com/product/TCA9554A).
+/// We therefore write a partial I2C driver for this device so we can easily work the LEDs.
+pub mod led {
+    use embedded_hal::i2c::I2c;
+    use esp_idf_svc::hal::prelude::*;
+
+    ///Baudrate Max clock frequency in KHz (depending if using standard or fast mode).
+    pub const BAUDRATE_STANDARD: KiloHertz = KiloHertz(100);
+    pub const BAUDRATE_FAST: KiloHertz = KiloHertz(400);
+
+    //Note I2c has the following definition:
+    // pub trait I2c<A: AddressMode = SevenBitAddress>: ErrorType  {..}
+    // So we don't need to specify SevenBitAddress here.
+
+
+    /// Note that this Register enum is also all possibilites for the command byte 
+    /// as it is only sent for *write* operations
+    /// (see 8.6.2 Control Register and Command Byte in the Data Sheet).
+    /// When sent to the IO expander after the addressing byte, this byte tells the IO expander
+    /// we wish to write to the corresponding register.
+    /// All these registers are 8-bit.
+    /// 
+    /// If we wanted to read a register instead, we would simply use the 
+    /// [write_read method](https://docs.rs/embedded-hal/latest/embedded_hal/i2c/trait.I2c.html#method.write_read)
+    /// which also matches the IO expander data sheet.
+    #[allow(unused)]
+    enum Register {
+        InputPort = 0,
+        OutputPort = 1,
+        PolarityInversion = 2,
+        Configuration = 3,
+    }
+
+    pub enum LEDs {
+        Green,
+        Blue,
+    }
+
+    pub struct LedDriver<I2C> {
+        i2c: I2C,
+
+        //tracking the state of the leds in this sturct saves us having to read IO expander registers.
+        led_1_on: bool,
+        led_2_on: bool,
+    }
+
+    impl<I2C: I2c> LedDriver<I2C> {
+        //Associated Constants
+
+        ///From the [Korvo schematic](https://dl.espressif.com/dl/schematics/SCH_ESP32-S3-Korvo-2_V3.1.2_20240116.pdf)
+        /// the I2C Address (of this io expander)：0'b 0111 000x .
+        /// This address is specified in the **right-aligned** form as specified
+        /// [here](https://docs.rs/embedded-hal/latest/src/embedded_hal/i2c.rs.html#296).
+        const ADDR: u8 = 0b00111000;
+
+        ///This is called P6 for the Korvo. On the IO expander it is pin 11.
+        /// Push-pull design structure.
+        const LED1_PIN_ON: u8 = 0b01_000000;
+        ///This is called P7 for the Korvo. On the IO expander it is pin 11.
+        /// Push-pull design structure.
+        const LED2_PIN_ON: u8 = 0b1_0000000;
+
+        ///The byte in each register is of the form: P7 P6 P5 P4 P3 P2 P1 P0 (MSB -> LSB).
+        /// So to clear P6 and P7 in the configuration register (sets the LEDs as output)
+        /// we need to write this byte
+        const SET_LEDS_OUTPUT: u8 = 0b00111111;
+
+        ///Creates and initializes the LED driver.
+        pub fn build(i2c: I2C) -> Self {
+            let mut driver = Self {
+                i2c,
+                led_1_on: false,
+                led_2_on: false,
+            };
+            driver
+                .init()
+                .expect("Setting LEDs as output should not fail");
+            driver
+        }
+
+        ///At power on, the IO expander device has all pins being inputs.
+        /// We set the LED pins as outputs.
+        /// We do this by clearing the relevant bits from the Configuration register (which starts with all bits set).
+        fn init(&mut self) -> Result<(), I2C::Error> {
+            self.i2c.write(Self::ADDR, &[Register::Configuration as u8, Self::SET_LEDS_OUTPUT])?;
+
+            //Once we enable the LEDs pins as outputs, their default values will turn them on.
+            self.led_1_on = true;
+            self.led_2_on = true;
+            Ok(())
+        }
+
+        pub fn flip_led(&mut self, led: LEDs) {
+            match led {
+                LEDs::Blue => todo!(),
+                LEDs::Green => todo!(),
+            }
+        }
+
+        pub fn get_led_states(&self) -> (bool, bool){
+            (self.led_1_on, self.led_2_on)
+        }
+    }
 }
