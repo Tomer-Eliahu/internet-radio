fb587e75bdbfc92c3b8ff2e0f320879ac1e8b338
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-09-18 23:18:31 +0300

    added example config

---
+++ src/wifi/example_config.rs
@@ -0,0 +1,6 @@
+//Be sure to rename this file to just config.rs once you change the info below
+
+///Your wifi network name. Also known as an SSID (Service Set Identifier).
+pub const WIFI_NAME: &'static str = "My_Wifi_Name";
+
+pub const WIFI_PASSWORD: &'static str = "My_Wifi_Password";
